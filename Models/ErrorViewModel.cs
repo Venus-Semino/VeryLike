51052c6d170f86d69b2e0a005f4be5b5ca6de4d1
venus semino <venus.semino@tecdesoftware>
2026-05-15 21:08:07 -0600

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VeryLike.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
