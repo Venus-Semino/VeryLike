a0dee6d71bcbdf4dca8374b20ad9299bf8aa26de
SeminoVenus <semino.venus@gmail.com>
2026-08-01 08:38:40 -0600

    FIX

--- Data/Migrations/00000000000000_CreateIdentitySchema.Designer.cs
+++ Data/Migrations/00000000000000_CreateIdentitySchema.Designer.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using VeryLike.Data;
+using VeryLike.Infrastructure.Data;
 
 namespace VeryLike.Data.Migrations
 {
--- Data/Migrations/ApplicationDbContextModelSnapshot.cs
+++ Data/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using VeryLike.Data;
+using VeryLike.Infrastructure.Data;
 
 namespace VeryLike.Data.Migrations
 {
